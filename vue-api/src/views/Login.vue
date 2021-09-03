af6778142d16fa1354987d1f44465dbb69268de5
Mohamedfadelzergou <mohamedfadelzergou@gmail.com>
2021-09-03 12:52:27 +0100

    add pages login register profil and add form login

---
+++ vue-api/src/views/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+      <div class>
+          <div class="col-md-6 offset-md-3">
+              <h2 class="display-4 text-center mt-5">
+                  Login Form
+              </h2>
+              <form action="" method="post">
+                  <div class="form-group">
+                      <label for="email">Email</label>
+                      <input type="email" class="form-control" name="email" id="email">
+                  </div>
+                  <div class="form-group">
+                      <label for="password">Password</label>
+                      <input type="password" class="form-control" name="password" id="password">
+                  </div>
+                  <div class="form-group">
+                      <button type="submit" class="btn btn-primary btn-block">Login</button>
+                  </div>
+              </form>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
